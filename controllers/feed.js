0bd08b956fbdd6c91f1189f35fdcff936cc573fa
Kantslo <gqancliani@gmail.com>
2024-04-23 20:31:46 +0400

    chore: small edition to post controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,9 +2,14 @@ export const getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: "1",
         title: "First post",
         content: "This is a first post!",
         imageUrl: "images/UFC-belt.jpg",
+        creator: {
+          name: "Giorgi",
+        },
+        createdAt: new Date(),
       },
     ],
   });
@@ -16,6 +21,12 @@ export const createPost = (req, res, next) => {
 
   res.status(201).json({
     message: "Post created successfully",
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: { name: "Giorgi" },
+      createdAt: new Date(),
+    },
   });
 };
