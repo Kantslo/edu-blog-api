f4279ff13d2dc64273d75fa1cade36bee19df19e
Kantslo <gqancliani@gmail.com>
2024-04-26 21:03:42 +0400

    feat: initialized multer middleware

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import multer from "multer";
+import uuid from "uuid";
 
 import connect from "./config/mongo.js";
 import feedRoutes from "./routes/feed.js";
@@ -13,7 +15,29 @@ connect();
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuid());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "images/png" ||
+    file.mimetype === "images/jpg" ||
+    file.mimetype === "jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json());
+app.use(multer({ storage: fileStorage, fileFilter }));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(cors());
