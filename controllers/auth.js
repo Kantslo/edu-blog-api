ab22a79b8e3241423c5130b17e720eb94e3ee802
Kantslo <gqancliani@gmail.com>
2024-05-01 18:49:35 +0400

    feat: added controller function to handle user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import { validationResult } from "express-validator";
 
@@ -26,6 +27,44 @@ export const signup = (req, res, next) => {
     })
     .then((result) => {
       res.status(201).json({ message: "User created", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+export const login = (req, res, next) => {
+  const { email, password } = req.body;
+  let loadedUser;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User with this email could not be found.");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong password!");
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        "supersecretsecretkey",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token, userId: loadedUser._id.toString() });
     })
     .catch((err) => {
       if (!err.statusCode) {
