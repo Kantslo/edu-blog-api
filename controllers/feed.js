b07b1446cd57a93f2a180f21358fe4fab4a0c208
Kantslo <gqancliani@gmail.com>
2024-05-07 22:56:10 +0400

    feat: added io instance on update post function

--- controllers/feed.js
+++ controllers/feed.js
@@ -56,7 +56,11 @@ export const createPost = async (req, res, next) => {
     const user = User.findById(req.userId);
     user.posts.push(post);
     await user.save();
-    getIO().emit("posts", { action: "create", post });
+    getIO().emit("posts", {
+      action: "create",
+      ...post._doc,
+      creator: { _id: req.userId, name: user.name },
+    });
     res.status(201).json({
       message: "Post created successfully",
       post,
@@ -110,13 +114,13 @@ export const updatePost = async (req, res, next) => {
   }
 
   try {
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate("creator");
     if (!post) {
       const error = new Error("Could not find post.");
       error.statusCode = 404;
       throw error;
     }
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error("Not authorized!");
       error.statusCode = 403;
       throw error;
@@ -128,6 +132,7 @@ export const updatePost = async (req, res, next) => {
     imageUrl.title = imageUrl;
     imageUrl.content = content;
     const result = await post.save();
+    getIO().emit("posts", { action: "update", post: result });
     res.status(200).json({ message: "Post updated!", post: result });
   } catch (err) {
     if (!err.statusCode) {
