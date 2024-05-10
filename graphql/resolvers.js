e49133e741e5f81f07628fb3ffc28e683c369128
Kantslo <gqancliani@gmail.com>
2024-05-10 17:39:05 +0400

    feat: added graphql resolver for user registration

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,24 @@
-export const hello = () => {
-  return {
-    text: "Hello, World!",
-    views: 12345,
-  };
+import bcrypt from "bcrypt";
+
+import User from "../models/user.js";
+
+const resolvers = {
+  createUser: async (args, req) => {
+    const { email, name, password } = args.userInput;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error("User exists already!");
+      throw error;
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      email,
+      name,
+      password: hashedPassword,
+    });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
 };
+
+export default resolvers;
