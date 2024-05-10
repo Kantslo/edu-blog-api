a76e76e75997d6638bcb7b4c4a5da87ead01b6c2
Kantslo <gqancliani@gmail.com>
2024-05-10 18:22:15 +0400

    feat: added validation in graphql resolver with validator package

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,25 @@
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 import User from "../models/user.js";
 
 const resolvers = {
   createUser: async (args, req) => {
     const { email, name, password } = args.userInput;
+    const errors = [];
+    if (!validator.isEmail(email)) {
+      errors.push({ message: "E-mail is invalid." });
+    }
+    if (
+      validator.isEmpty(password) ||
+      !validator.isLength(password, { min: 5 })
+    ) {
+      errors.push({ message: "Password is too short!" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      throw error;
+    }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       const error = new Error("User exists already!");
