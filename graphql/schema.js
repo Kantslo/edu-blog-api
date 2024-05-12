5fab314be8a4d48ce9be782b388fc6834f683571
Kantslo <gqancliani@gmail.com>
2024-05-12 23:25:38 +0400

    feat: added pagination logic

--- graphql/schema.js
+++ graphql/schema.js
@@ -44,7 +44,7 @@ const schema = buildSchema(`
 
   type RootQuery {
     login(email: String!, password: String!): AuthData!
-    posts: PostData!
+    posts(page: Int!): PostData!
   }
 
   type RootMutation {
