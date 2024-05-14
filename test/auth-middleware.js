6ad9f1afdccea863fbaf004439e41ac995cb1b9a
Kantslo <gqancliani@gmail.com>
2024-05-15 03:51:10 +0400

    feat: added describe method to organize my tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,24 +2,26 @@ import { expect } from "chai";
 
 import authMiddleware from "../middlewares/auth.js";
 
-it("should throw an error if no authorization file is present", function () {
-  const req = {
-    get: function (headerName) {
-      return null;
-    },
-  };
-  const next = () => {};
-  authMiddleware(req, {}, next);
+describe("Auth middleware", function () {
+  it("should throw an error if no authorization file is present", function () {
+    const req = {
+      get: function (headerName) {
+        return null;
+      },
+    };
+    const next = () => {};
+    authMiddleware(req, {}, next);
 
-  expect(req).to.have.property("isAuth", false);
-});
+    expect(req).to.have.property("isAuth", false);
+  });
 
-it("should throw an error if the authorization header is only one string", function () {
-  const req = {
-    get: function (headerName) {
-      return "Bearer";
-    },
-  };
-  const next = () => {};
-  expect(authMiddleware.bind(this, req, {}, next)).to.throw();
+  it("should throw an error if the authorization header is only one string", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer";
+      },
+    };
+    const next = () => {};
+    expect(authMiddleware.bind(this, req, {}, next)).to.throw();
+  });
 });
